fcbeaece0c0f6a85a950526da6a01d07a6fa0898
Zubairu Awudu <zubairuawudu123@gmail.com>
2024-12-03 23:44:54 -0500

    JS-LabAssignment

--- app.js
+++ app.js
@@ -151,14 +151,52 @@ for(let i = 12; i < 4000; i+=3)
 //1. Print out the numbers that are within the range of 1 - 100
 //2. Adjust your code to add a message next to even numbers only that says: "<-- is an even number" 
 //-------------SOLUTION ----------- //
-for (let i = 1; i <= 100; i++){
-  if (i % 2 === 0){
-    console.log(i + " <-- is an even number");
-  }
+// for (let i = 1; i <= 100; i++){
+//   if (i % 2 === 0){
+//     // console.log(`${i} <-- is an even number`);
+//   } else{
+//     // console.log(i);
+
+//   }
+// }
+
+
+// C. Give me Five
+
+//1.For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five
+// 2. Add to the code from above to print out "I found a number. Three is a crowd" if the number is a multiple of three
+//3. For numbers divisible by both three and five, be sure your code prints both messages
+
+//-------------SOLUTION-----------------//
+// for (let i = 0; i <= 100; i++){
+//   let highFive = 5
+//   let crowd = 3
+//   if (i % 5 === 0){
+//     console.log(`I found a ${highFive}. High five!`);
+//   }else if(i % 3 === 0){
+//     console.log(`I found a ${crowd}. Three is a crowd`);
+    
+//   }else{
+//     console.log(`I found a ${crowd}. Three is a crowd , I found a ${highFive}. High five! `);
+//   }
+// }
+
+
+// D. Savings account
+
+//Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account.
+//Check your work! Your bank_account should have $55 in it.
+//You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
+//Check your work! Your bank_account should have $10,100 in it.
+
+//--------------- SOLUTION ---------------------- //
+// to calculate the sum of numbers
+
+let sum = 0;
+for(let i = 1; i <=10; i++ ){
+  bank_account += i;
 }
 
-
-// 
 
 
 
